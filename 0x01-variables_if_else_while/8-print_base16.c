fd6bd6aa5fc9a9c7506c86c85ffc11635484f521
Lindiwemojela <lindiwepkmojela@gmail.com>
2023-02-19 03:21:00 -0800

    My 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -19,7 +19,7 @@ int main(void)
 			d++;
 		}
 	while
-		(c <= 'd') {
+		(c <= 'f') {
 			putchar(c);
 			c++;
 		}
