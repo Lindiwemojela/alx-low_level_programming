593284a056cecc12f438c04bce8eda37b2936fcd
Lindiwemojela <lindiwepkmojela@gmail.com>
2023-02-23 01:53:19 -0800

    My 2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 
 {
 	char c;
