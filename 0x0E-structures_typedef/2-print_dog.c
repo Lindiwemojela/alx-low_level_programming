c78fc8bad4a3af2a161db6caf24ff7a27c60028a
Lindiwemojela <lindiwepkmojela@gmail.com>
2023-04-04 17:43:03 -0700

    My 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * main - Check the code
- *
- * Return: Always 0 (Success)
+ * print_dog - Prints a struct dog
+ * @d: Struct dog to print
  */
-int main(void)
-{
-	struct dog my_dog;
 
-	my_dog.name = "Poppy";
-	my_dog.age = 3.5;
-	my_dog.owner = "Bob";
-	print_dog(&my_dog);
-	return (0);
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner:
+	%s\n", d->name, d->age, d->owner);
+
 }
